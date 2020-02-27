9def062ba7ab3268a69f7b269850ba40e9ec2f9f
nilson.silva <nilson.silva@sankhya.com.br>
2020-02-27 00:54:12 -0300

    Primeiro commit/ configurando o GULP

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp"),
+    imagemin = require("gulp-imagemin"),
+    clean = require("gulp-clean");
+
+gulp.task("copy", ["clean"], function () {
+    return gulp.src("src/img/**/*")
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task("clean", function () {
+    return gulp.src("dist")
+        .pipe(clean());
+});
+
+gulp.task('build-img', ["copy"], function () {
+    gulp.src("dist/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+
+
